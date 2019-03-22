cedafb2ff3bf867a2c8240ad661a7615ecb883b6
Your Name <you@example.com>
2019-03-23 01:27:36 +0530

    as_dict

--- time_tracking/time_tracking/customer_item_code.py
+++ time_tracking/time_tracking/customer_item_code.py
@@ -6,5 +6,6 @@ from frappe import _, msgprint, throw
 
 @frappe.whitelist()
 def getCustomerItemCode(item,customer):
-	customer_item_code = frappe.db.sql("select icd.ref_code from `tabItem Customer Detail` icd where icd.parent = '{0}' and icd.customer_name = '{1}';".format(item,customer),as_list=1)
-	return customer_item_code
+	customer_item_code = frappe.db.sql("select icd.ref_code from `tabItem Customer Detail` icd where icd.parent = '{0}' and icd.customer_name = '{1}';".format(item,customer),as_dict = True)
+	code = customer_item_code[0]
+	return code
